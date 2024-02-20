d7d350f95e0283d094cedc728f0cd6c00643ddaf
Jasin <jasinmarkuu@gmail.com>
2024-02-20 10:26:18 -0500

    Initial Commit

---
+++ XCoin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCoin
+//
+//  Created by Jasin ‎ on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCoin/XCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCoinApp.swift
+//  XCoin
+//
+//  Created by Jasin ‎ on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct XCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
